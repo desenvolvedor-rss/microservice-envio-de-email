a7b545eccd00d5d2e23c26c481bf35b6796f0346
ProjetoPneuStore <91225546+ProjetoPneuStore@users.noreply.github.com>
2023-06-24 16:49:13 -0300

    Initial commit

---
+++ src/main/java/com/microservice/email/controllers/EmailController.java
@@ -0,0 +1,29 @@
+package com.microservice.email.controllers;
+
+
+import com.microservice.email.dtos.EmailDto;
+import com.microservice.email.models.EmailModel;
+import com.microservice.email.service.EmailService;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmailController {
+
+    @Autowired
+    EmailService emailService;
+
+    @PostMapping("/enviando-email")
+    public ResponseEntity<EmailModel> sendingEmail(@RequestBody @Valid EmailDto emailDto){
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailDto, emailModel);
+        emailService.sendEmail(emailModel);
+        return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
+    }
+}
